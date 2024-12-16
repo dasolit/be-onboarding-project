e3f159e1c6f7ed50ec814afb0bc9ce50eb282f68
itmdeveloper <choidasol95@gmail.com>
2024-12-16 21:00:28 +0900

    api 모듈 service error 수정,  jpa repo에 @repository 어노테이션 추가

--- survey/api/src/main/java/com/fc/api/ApiApplication.java
+++ survey/api/src/main/java/com/fc/api/ApiApplication.java
@@ -3,7 +3,7 @@ package com.fc.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.fc.api", "com.fc.core", "com.fc.domain"})
 public class ApiApplication {
 
   public static void main(String[] args) {
--- survey/domain/src/main/java/com/fc/domain/repository/QuestionRepository.java
+++ survey/domain/src/main/java/com/fc/domain/repository/QuestionRepository.java
@@ -2,7 +2,9 @@ package com.fc.domain.repository;
 
 import com.fc.domain.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 }
